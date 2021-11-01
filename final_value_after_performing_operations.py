14913602cfc22b1b5bde5780dcb06666957f41ae
Eleni Mandana <elemandana@gmail.com>
2021-11-02 00:19:30 +0200

    commit

---
+++ final_value_after_performing_operations.py
@@ -0,0 +1,53 @@
+# CORRECT 100% 
+# Runtime: 40 ms, faster than 99.32% of Python3 online submissions for Final Value of Variable After Performing Operations.
+# Memory Usage: 14.4 MB, less than 17.25% of Python3 online submissions for Final Value of Variable After Performing Operations.
+
+"""
+DESCRIPTION:
+There is a programming language with only four operations and one variable X:
+
+    ++X and X++ increments the value of the variable X by 1.
+    --X and X-- decrements the value of the variable X by 1.
+
+Initially, the value of X is 0.
+Given an array of strings operations containing a list of operations, return the final value of X after performing all the operations.
+
+
+TEST CASES:
+Input: operations = ["--X","X++","X++"]
+Output: 1
+Explanation: The operations are performed as follows:
+Initially, X = 0.
+--X: X is decremented by 1, X =  0 - 1 = -1.
+X++: X is incremented by 1, X = -1 + 1 =  0.
+X++: X is incremented by 1, X =  0 + 1 =  1.
+
+Input: operations = ["++X","++X","X++"]
+Output: 3
+Explanation: The operations are performed as follows:
+Initially, X = 0.
+++X: X is incremented by 1, X = 0 + 1 = 1.
+++X: X is incremented by 1, X = 1 + 1 = 2.
+X++: X is incremented by 1, X = 2 + 1 = 3.
+
+Input: operations = ["X++","++X","--X","X--"]
+Output: 0
+Explanation: The operations are performed as follows:
+Initially, X = 0.
+X++: X is incremented by 1, X = 0 + 1 = 1.
+++X: X is incremented by 1, X = 1 + 1 = 2.
+--X: X is decremented by 1, X = 2 - 1 = 1.
+X--: X is decremented by 1, X = 1 - 1 = 0.
+"""
+
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        ans = 0
+        
+        for op in operations:
+            if op.count('+'):
+                ans = ans + 1
+            else:
+                ans = ans - 1
+        
+        return ans
