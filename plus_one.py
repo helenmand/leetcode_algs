b24bfafe8a55af2b7988216ebc5382d107dfa80f
Eleni Mandana <elemandana@gmail.com>
2022-02-23 17:22:03 +0200

    plus one added

---
+++ plus_one.py
@@ -0,0 +1,56 @@
+# CORRECT 100%
+# Runtime: 59 ms, faster than 18.84% of Python3 online submissions for Plus One.
+# Memory Usage: 13.9 MB, less than 84.89% of Python3 online submissions for Plus One.
+
+"""
+DESCRIPTION:
+You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. 
+The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+Increment the large integer by one and return the resulting array of digits. 
+Constraints: 1 <= digits.length <= 100, 0 <= digits[i] <= 9 & digits does not contain any leading 0's.
+
+TEST CASES:
+Input: digits = [1,2,3]
+Output: [1,2,4]
+Explanation: The array represents the integer 123.
+Incrementing by one gives 123 + 1 = 124.
+Thus, the result should be [1,2,4].
+
+Input: digits = [4,3,2,1]
+Output: [4,3,2,2]
+Explanation: The array represents the integer 4321.
+Incrementing by one gives 4321 + 1 = 4322.
+Thus, the result should be [4,3,2,2].
+
+Input: digits = [9]
+Output: [1,0]
+Explanation: The array represents the integer 9.
+Incrementing by one gives 9 + 1 = 10.
+Thus, the result should be [1,0].
+"""
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        
+        res = []
+        add = True
+
+        for digit in digits[::-1]:
+            if add is True:
+                digit = digit + 1
+            
+            if digit//10 == 0:
+                add =  False
+            
+            if add is True:
+                res.append(0)
+            else:
+                res.append(digit)
+        
+        # the last digit we added 1 to, is 9, the result is 10 and only 0 has been added
+        # there are no more digits in the array and it has to be done here.
+        if add is True:
+            res.append(1)
+            
+        return res[::-1]
