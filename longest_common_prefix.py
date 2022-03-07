a7d8388c50adfa0b88c4c0c99a08ee0218b489fb
Eleni Mandana <elemandana@gmail.com>
2022-03-07 19:24:45 +0200

    longest common prefix

---
+++ longest_common_prefix.py
@@ -0,0 +1,37 @@
+# CORRECT 100%
+# Runtime: 49 ms, faster than 52.89% of Python3 online submissions for Longest Common Prefix. 
+# Memory Usage: 13.9 MB, less than 71.13% of Python3 online submissions for Longest Common Prefix.
+
+"""
+DESCRIPTION:
+Write a function to find the longest common prefix string amongst an array of strings.
+If there is no common prefix, return an empty string "".
+
+
+TEST CASES:
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+ 
+
+Constraints:
+1 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] consists of only lower-case English letters.
+"""
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        res = ""
+        
+        for letter in max(strs):
+            res += letter
+            
+            for word in strs:
+                if not word.startswith(res):
+                    res = res[:-1]
+                    return res
+        return res
