b97e908bca6c8604e2e8411693dea51fcedd6560
Eleni Mandana <elemandana@gmail.com>
2022-07-22 19:04:23 +0300

    second highest salary - 176

---
+++ medium - sql/176.second_highest_salary.sql
@@ -0,0 +1,51 @@
+"""
+DESCRIPTION:
+Table: Employee
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| salary      | int  |
++-------------+------+
+id is the primary key column for this table.
+Each row of this table contains information about the salary of an employee.
+Write an SQL query to report the second highest salary from the Employee table. If there is no second highest salary, the query should report null.
+The query result format is in the following example.
+
+
+TEST CASES:
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+Output: 
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| 200                 |
++---------------------+
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
++----+--------+
+Output: 
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| null                |
++---------------------+
+"""
+select 
+ifnull(
+    (select distinct salary as SecondHighestSalary
+    from Employee
+    order by salary desc limit 1, 1), null) as SecondHighestSalary;
